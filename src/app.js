b87a869433e9824bf31548bca92444db5ff9f870
timua92 <tim.edwards@ua92.ac.uk>
2025-09-10 16:39:23 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ app.get('/health', (req, res) => {
 });
 
 app.get('/', (req, res) => {
-  res.send('Hello, CI/CD! My next is finally complete after manfghfhy errors!');
+  res.send('Hello, CI/CD! My project is ready!');
 });
 
 module.exports = app;
